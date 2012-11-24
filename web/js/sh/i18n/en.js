b197883390df630daa359687da689a1767687a5e
TheCelavi <thecelavi@gmail.com>
2012-11-24 18:50:02 +0100

    removed sufficient translation

--- web/js/sh/i18n/en.js
+++
@@ -1,8 +0,0 @@
-if (!dm_configuration.i18n) dm_configuration.i18n = { };
-dm_configuration.i18n.SyntaxHighlighter = {
-    expandSource: '+ expand source',
-    help: '?',
-    alert: 'SyntaxHighlighter\n\n',
-    noBrush: 'Can\'t find brush for:',
-    brushNotHtmlScript: 'Brush wasn\'t made for html-script option:'
-};
